7b34547efe4701acba18f84814083de6090c8834
jayashreebalinani <jayashreebalinani@gmail.com>
2023-05-31 12:39:42 +0530

    first commit

---
+++ port.js
@@ -0,0 +1,31 @@
+
+let banner = document.querySelector('.banner')
+let darkBtn = document.querySelector('#dark')
+let lightBtn = document.querySelector('#light')
+
+
+// ======= Typed Effect=======
+
+let typingText = new Typed('#text', {
+    strings: ["Vxrcel", "Designer", "Pulse", "Gamer", "Wudan"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+})
+
+
+let myTheme = localStorage.getItem('theme')
+
+if (myTheme) {
+    document.body.className = myTheme
+}
+lightBtn.onclick = () => {
+    document.body.className = 'dark'
+    localStorage.setItem('theme', 'dark')
+}
+darkBtn.onclick = () => {
+    document.body.className = 'light'
+    localStorage.setItem('theme', 'light')
+
+}
